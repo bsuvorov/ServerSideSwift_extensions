31ab96d9eb2461b4904ae7856cd894fc8ee4c045
Nikolay Borysov <niks.borisov@gmail.com>
2018-04-06 13:25:05 +0300

    Make dateFormatters public.

--- Sources/ServerSideSwiftExtensions/DateFormatters.swift
+++ Sources/ServerSideSwiftExtensions/DateFormatters.swift
@@ -2,13 +2,13 @@ import Foundation
 
 public class DateFormatters {
     
-    private(set) lazy var BackendDateFormat: DateFormatter = {
+    public private(set) lazy var BackendDateFormat: DateFormatter = {
         var formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter
     }()
     
-    private(set) lazy var USDateFormat: DateFormatter = {
+    public private(set) lazy var USDateFormat: DateFormatter = {
         var formatter = DateFormatter()
         formatter.dateFormat = "MM/dd/yyyy"
         return formatter
